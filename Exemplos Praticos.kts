ed8554a332f8325cc4aee227274941aeb1066ee8
Luis Felipe <felipeadriani2@gmail.com>
2022-12-20 08:38:57 -0300

    Classes

--- Exemplos Praticos.kts
+++ Exemplos Praticos.kts
@@ -132,3 +132,54 @@ fun main(){
 	println(describeString("Luis"))
 
 }
+
+
+//Classes
+class Contato(val id: Int, var email: String)
+
+fun main() {
+	val cliente = Cliente()
+    
+    val contato = Contato(1, "felipe@gmail.com")
+    
+    println(contato.email)
+    println(contato.id)
+    contato.email = "luis@gmail.com"
+    println(contato.email)
+
+}
+
+//Generics- CLasses Genericas
+
+class MutableStack<E>(vararg items : E){
+    
+    private val elements = items.toMutableList()
+    
+    fun push(element: E) = elements.add(element)
+    
+    fun peek(): E = elements.last()
+    
+    fun pop(): E = elements.removeAt(elements.size - 1)
+    
+    fun isEmpty() = elements.isEmpty()
+    
+    fun size() = elements.size
+    
+    override fun toString() = "MutableStack(${elements.joinToString()})"
+}
+
+fun main() {
+    val stack = MutableStack(0.62, 3.14, 2.7)
+	stack.push(9.87)
+    println(stack)
+    
+    println("peek(): ${stack.peek()}")
+    println(stack)
+    
+    for (i in 1..stack.size()) {
+        println("pop(): ${stack.pop()}")
+        println(stack)
+    }
+}
+
+
