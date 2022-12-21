25a7e521bdaa9ea5dd05d6e177f2eb8a5aea81de
Luis Felipe <felipeadriani2@gmail.com>
2022-12-21 15:50:17 -0300

    Ranges

--- Controle de Fluxo.kts
+++ Controle de Fluxo.kts
@@ -81,7 +81,7 @@ fun main(args: Array<String>) {
 class Animal(val name: String)
 
 class Zoo(val animals: List<Animal>) {
-    operator fun interator(): Iterator<Animal> {
+    operator fun iterator(): Iterator<Animal> {
         return animals.iterator()
     }
 }
@@ -92,5 +92,61 @@ fun main() {
 
     for (animal in zoo) {
         println("Watch out, it's a ${animal.name}")
+    }
+}
+
+
+//Ranges: Loops com Int
+
+fun main () {
+    for(i in 0..3) {
+        print(i)
+    }
+    print(" ")
+
+    for(i in 0 until 3) {
+        print(i)
+    }
+    print(" ")
+
+    for(i in 2..8 step 2) {
+        print(i)
+    }
+    print(" ")
+
+    for(i in 3 downTo 0) {
+        print(i)
+    }
+    print(" ")
+    
+}
+
+
+//Ranges: Ifs e loops com Char
+
+fun main(){
+
+    for (c1 in 'a'..'d') {
+        print(c1)
+    }
+    print(" ")
+
+    for (c2 in 'z' downTo 's' step 2) {
+        print(c2)
+    }
+    print(" ")
+}
+
+
+fun main(){
+
+    val x = 2
+    if (x in 1..5) {
+        print("x is in range from 1 to 5")
+    }
+    println()
+
+    if (x !in 6..10) {
+        print("x is not in range from 6 to 10")
     }
 }
