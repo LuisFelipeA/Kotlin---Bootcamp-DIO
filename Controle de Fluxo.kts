b03e8c87bb00a9641b35f257fdcabfd583f6f0e3
Luis Felipe <felipeadriani2@gmail.com>
2022-12-21 16:03:48 -0300

    Expressão Condicional

--- Controle de Fluxo.kts
+++ Controle de Fluxo.kts
@@ -161,3 +161,11 @@ fun main() {
     println(authors == writers)
     println(authors === writers)
 }
+
+
+// Expressão Condicional
+
+fun main() {
+    fun max(a: Int, b: Int) = if (a > b) a else  b
+    println(max(99, -42))
+}
