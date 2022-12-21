aaa41275cc50baf23f17ad16f2046b5ddcee4828
Luis Felipe <felipeadriani2@gmail.com>
2022-12-21 15:32:52 -0300

    Loops

--- Controle de Fluxo.kts
+++ Controle de Fluxo.kts
@@ -41,3 +41,56 @@ fun whenAssign(obj: Any): Any {
 }
 
 class MyClass
+
+
+//Loops:For
+
+fun main() {
+
+    val cakes = listOf("carrot", "cheese", "chocolate")
+
+    for (cake in cakes) {
+        println("Yummy, its a $cake cake")
+    }
+}
+
+
+//Loops: While e do while
+
+fun eatACake() = println("Eat a Cake")
+fun bakeACake() = println("Bake a Cake")
+
+fun main(args: Array<String>) {
+    var cakesEaten = 0
+    var cakesBaked = 0
+
+    while (cakesEaten < 5) {
+        eatACake()
+        cakesEaten ++
+    }
+
+    do {
+        bakeACake()
+        cakesBaked ++
+    } while (cakesBaked < cakesEaten)
+}
+
+
+//Loops:Iterators
+
+class Animal(val name: String)
+
+class Zoo(val animals: List<Animal>) {
+    operator fun interator(): Iterator<Animal> {
+        return animals.iterator()
+    }
+}
+
+fun main() {
+    
+    val zoo = Zoo(listOf(Animal("zebra"),Animal("lion")))
+
+    for (animal in zoo) {
+        println("Watch out, it's a ${animal.name}")
+    }
+}
