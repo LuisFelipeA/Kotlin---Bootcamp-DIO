0723a300e14c47de2db804e6ab493fd583b458a5
Luis Felipe <felipeadriani2@gmail.com>
2022-12-21 14:37:40 -0300

    Concluido

--- Exemplos Praticos.kts
+++ Exemplos Praticos.kts
@@ -183,3 +183,28 @@ fun main() {
 }
 
 
+//Generics - Funções Genericas
+
+class MutableStack<E>(vararg items : E){
+    
+    private val elements = items.toMutableList()
+    
+    fun push(element: E) = elements.add(element)
+    
+    fun peek(): E = elements.last()
+    
+    fun pop(): E = elements.removeAt(elements.size - 1)
+    
+    fun isEmpty() = elements.isEmpty()
+    
+    fun size() = elements.size
+    
+    override fun toString() = "MutableStack(${elements.joinToString()})"
+}
+
+fun <E> mutableStackOf(vararg elements: E) = MutableStack(*elements)
+
+fun main() {
+    val stack = mutableStackOf(62, 14, 7)
+    println(stack)
+}
