c17aa42738a843b8df6b579a46cbc2aabd6e404d
Luis Felipe <felipeadriani2@gmail.com>
2022-12-28 08:06:49 -0300

    Data Classes

--- Programação Orientada a Objetos.kts
+++ Programação Orientada a Objetos.kts
@@ -57,6 +57,33 @@ fun main () {
 
 //Data Classes (Encapsulamento)
 
+data class User(val name: String, val id: Int) {
+    override fun equals (other: Any?) =
+        other is User && other.id == this.id
+}
+
+fun main() {
+    val user = User("Alex", 1)
+    println(user)
+
+    val secondUser = User("Alex", 1)
+    val thirdUser = User("Max", 2)
+
+    println("user == secondUser: ${user == secondUser}")
+    println("user == thirdUser: ${user == thirdUser}")
+
+    //hasCode() function
+    println(user.hashCode())
+    println(secondUser.hashCode())
+    println(thirdUser.hashCode())
+    
+    println(user.copy())
+    println(user === user.copy())
+    println(user.copy("Max"))
+    println(user.copy(id = 3))
+}
+
+
 //Enum Classes
 
 //Sealed Classes (Polimorfismo)
